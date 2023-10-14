525a75708e36fbbd9e83528451764f82630d1193
Rogério <rogeriopiatek@gmail.com>
2023-10-14 00:15:46 -0300

    minor changes on button styling

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,8 @@
 let button = document.querySelector(".toggle-theme");
 let body = document.querySelector("body");
 let clicked = true;
-button.addEventListener("click", () => {
+button.addEventListener("click", (e) => {
+  e.preventDefault;
   if (clicked === false) {
     button.classList = "toggle-theme button-dark";
     body.classList = "body bg-dark";
