48ecab6b35ecfd0d9096dcbbacbf7755dd2af1e0
Rogério <rogeriopiatek@gmail.com>
2023-10-14 00:11:34 -0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+let button = document.querySelector(".toggle-theme");
+let body = document.querySelector("body");
+let clicked = true;
+button.addEventListener("click", () => {
+  if (clicked === false) {
+    button.classList = "toggle-theme button-dark";
+    body.classList = "body bg-dark";
+    return (clicked = true);
+  }
+  if (clicked === true) {
+    button.classList = "toggle-theme button-white";
+    body.classList = "body bg-white";
+    return (clicked = false);
+  }
+});
